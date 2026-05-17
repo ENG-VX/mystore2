97ed56dbb67b3fb0676984c7c9f019d9a07cd899
ENG-VX <abdullahwork5757@gmail.com>
2026-05-17 20:21:48 +0900

    7- Creating API

--- myStore2/urls.py
+++ myStore2/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import store
 import debug_toolbar
 
 admin.site.site_header = 'Storefront Admin'
@@ -23,5 +24,6 @@ admin.site.index_title = 'Admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
+    path("store/", include('store.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
